a75d9e67a3d622cc8e498fc5634297e8e1e73eff
Grbiest <107209428+Grbiest@users.noreply.github.com>
2024-01-29 12:43:49 -0500

    Delete Battleship/ScoreBoard.py

--- Battleship/ScoreBoard.py
+++
@@ -1,9 +0,0 @@
-class ScoreBoard:
-
-    def __init__(self) -> None:
-        self.cpuScore = 0
-        self.humanScore = 0
-        pass
-
-    def __str__(self) -> str:
-        return("------CPU: " + str(self.cpuScore) + "--Human:" + str(self.humanScore) + "------")
